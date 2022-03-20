c4ae15801d614061973b861b981a773eda9b7d5e
Osama Ataullah <61313346+osamaataullah@users.noreply.github.com>
2022-03-21 03:21:25 +0530

    home screen and product details screen added

--- app/src/main/java/com/example/xchange/MainActivity.java
+++ app/src/main/java/com/example/xchange/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.xchange;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.xchange.R;
+
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    com.example.xchange.Adapter adapter;
+    ArrayList<String> items;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("First CardView Item");
+        items.add("Second CardView Item");
+        items.add("Third CardView Item");
+        items.add("Fourth CardView Item");
+        items.add("Fifth CardView Item");
+        items.add("Sixth CardView Item");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new com.example.xchange.Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+
+
+
     }
 }
